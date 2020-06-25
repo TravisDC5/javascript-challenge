98f4d3ff6c3cdd2eddb17819d0896caac8f09691
TravisDC5 <61341968+TravisDC5@users.noreply.github.com>
2020-06-25 10:58:15 -0700

    did the first search

--- static/js/app.js
+++ static/js/app.js
@@ -2,3 +2,35 @@
 var tableData = data;
 
 // YOUR CODE HERE!
+let tbody = d3.select("tbody");
+
+function buildTable(data){
+
+    tbody.html("");
+
+    data.forEach((dataRow) => {
+
+        let row = tbody.append("tr");
+               
+        Object.values(dataRow).forEach((val) => {
+           let cell = row.append("td");
+           cell.text(val);
+       });
+    })
+}
+
+function handleClick(){
+    d3.event.preventDefault();
+
+    let date = d3.select("#datetime").property("value");
+    let filterData = tableData;
+
+    if(date) {
+        filterData = filterData.filter((row) => row.datetime === date);
+    }
+    buildTable(filterData);
+}
+
+d3.selectAll("#filter-btn").on('click', handleClick);
+
+buildTable(tableData);
